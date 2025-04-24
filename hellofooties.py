8cf182c26127cc0cd652888c427900c190968777
adnile22 <159518362+adnile22@users.noreply.github.com>
2025-04-24 13:56:25 +0530

    Update hellofooties.py

--- hellofooties.py
+++ hellofooties.py
@@ -1,1 +1,3 @@
-print(" Hello Football Lovers")
+print("  Hello MMIT This is the Best Football Team In Our SPPU University")
+print(" And This Time We Winning The University Trophy")
+print("Vamos Vamos!!!!!!!!!!!!!!!!!!!!")
